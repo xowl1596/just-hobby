dea12b3a3c4661ed86c78b98eb04763ba8e5c9f2
Tae Ji Jang <xowl1596@gmail.com>
2019-08-06 09:26:45 +0900

    p5.js - 푸리에 변환 추가

---
+++ p5-codes/fourier/main.js
@@ -0,0 +1,52 @@
+let time = 0;
+let wave = [];
+
+function setup(){
+    createCanvas(600, 400);
+}
+
+function draw(){
+    background(0);
+    translate(200, 200);
+
+    let x = 0;
+    let y = 0;
+    
+    for(let i = 0; i < 100; i++){
+        let prevx = x;
+        let prevy = y;
+        let n = i * 2 + 1;
+        let radius = 50 * (4 / (n * PI));
+        x += radius * cos(n * time);
+        y += radius * sin(n * time);
+       
+
+        stroke(255);
+        noFill();
+        ellipse(prevx,prevy,radius * 2);
+
+        fill(255);
+        stroke(255);
+        line(prevx,prevy,x,y);
+        // ellipse(x,y,8);
+
+        
+    }   
+
+    wave.unshift(y);
+
+    translate(200, 0);
+    line(x-200,y,0,wave[0]);
+    beginShape();
+    noFill();
+    for(let i = 0; i < wave.length; i++){
+        vertex(i, wave[i]);
+    }
+    endShape();
+
+    time += 0.05;
+    
+    if(wave.length > 250){
+        wave.pop();
+    }
+}
